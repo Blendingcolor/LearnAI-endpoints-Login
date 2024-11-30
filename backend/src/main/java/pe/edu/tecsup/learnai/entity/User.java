414188948401a03d8fbacc4ce2324f28ef665460
Blendyn <becerraorellana6@gmail.com>
2024-11-30 18:46:07 -0500

    Entities implementation

--- backend/src/main/java/pe/edu/tecsup/learnai/entity/User.java
+++ backend/src/main/java/pe/edu/tecsup/learnai/entity/User.java
@@ -7,20 +7,22 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @Entity
-@Table(name = "users", uniqueConstraints = {
-        @UniqueConstraint(columnNames = "username"),
-        @UniqueConstraint(columnNames = "email")
-})
+@Table(name = "users")
 public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
+    @Column(name = "username", unique = true, length = 100)
     private String username;
+    @Column(name = "email", nullable = false, unique = true, length = 100)
     private String email;
+    @Column(name = "password", length = 255)
     private String password;
+    @Column(name = "verification_token")
     private String verificationToken;
+    @Column(name = "is_verified")
     private boolean isVerified;
 
     public User(String username, String email, String password, String verificationToken, boolean isVerified) {
